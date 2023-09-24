d022c050398d352cefe278b22cd908916510e020
naufaldzakiim <naufalimtiyaz35@gmail.com>
2023-09-24 13:16:37 +0700

    pretify

--- delete_cart.php
+++ delete_cart.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 session_start();
 
 if (isset($_SESSION['cart'])) {
-    unset($_SESSION['cart']);
+  unset($_SESSION['cart']);
 }
 
 header('Location: show_cart.php');
--- logout.php
+++ logout.php
@@ -1,10 +1,9 @@
-<?php 
+<?php
 session_start();
 
 if (isset($_SESSION['username'])) {
-    unset($_SESSION['username']);
-    session_destroy();
+  unset($_SESSION['username']);
+  session_destroy();
 }
 
 header('Location: login.php');
-?>
