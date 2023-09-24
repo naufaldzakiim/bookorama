be770cbfbba36abf267bb3ee827537589911fef0
naufaldzakiim <naufalimtiyaz35@gmail.com>
2023-09-24 21:33:48 +0700

    pretify & fix card header

--- lib/db_login.php
+++ lib/db_login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $db_host = '10.10.10.10';
 $db_database = 'bookorama2';
 $db_username = 'pbp';
@@ -6,13 +6,13 @@ $db_password = 'MENUGAS';
 
 $db = new mysqli($db_host, $db_username, $db_password, $db_database);
 if ($db->connect_errno) {
-    die("Could not connect to the database: <br />" . $db->connect_error);
+  die("Could not connect to the database: <br />" . $db->connect_error);
 }
 
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
-?>
