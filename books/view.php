1c7b183f9a10441c9615dc02eb565db421ed0f5a
naufaldzakiim <naufalimtiyaz35@gmail.com>
2023-09-24 19:03:57 +0700

    filter book [unfinished]

--- books/view.php
+++ books/view.php
@@ -1,9 +1,47 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+  header('Location: login.php');
+  exit;
+}
+
+require_once('../lib/db_login.php');
+
+if (isset($_GET['submit'])) {
+  $search = $_GET['search'];
+  $query = "SELECT b.isbn as isbn, b.author as author, b.title as title, c.name as category, b.price as price
+            FROM books b, categories c
+            WHERE b.categoryid = c.categoryid AND
+            (b.isbn LIKE '%$search%' OR
+            b.author LIKE '%$search%' OR
+            b.title LIKE '%$search%' OR
+            c.name LIKE '%$search%' OR
+            b.price LIKE '%$search%')";
+} else {
+  $query = "SELECT b.isbn as isbn, b.author as author, b.title as title, c.name as category, b.price as price
+            FROM books b, categories c
+            WHERE b.categoryid = c.categoryid";
+}
+
+$result = $db->query($query);
+  if (!$result) {
+    die("Could not query the database: <br />" . $db->error . '<br>Query: ' . $query);
+  }
+
+?>
+
 <?php include('../header.php') ?>
 <div class="card mt-3">
   <div class="card-header">Books Data</div>
   <div class="card-body">
-    <a href="add.php" class="btn btn-primary mb-3">+ Add New Book</a>
-    <br>
+    <div class="mb-3 d-flex justify-content-between">
+      <a href="add.php" class="btn btn-primary">+ Add New Book</a>
+      <form action="" class="d-flex" method="GET">
+        <input id="search" class="form-control me-2" type="text" placeholder="Search book">
+        <button class="btn btn-outline-primary" type="submit" value="submit">Search</button>
+      </form>
+    </div>
     <table class="table table-striped">
       <tr>
         <th>ISBN</th>
@@ -14,16 +52,6 @@
         <th>Action</th>
       </tr>
       <?php
-      require_once('../lib/db_login.php');
-
-      $query = "SELECT b.isbn as isbn, b.author as author, b.title as title, c.name as category, b.price as price
-                FROM books b, categories c
-                WHERE b.categoryid = c.categoryid";
-      $result = $db->query($query);
-      if (!$result) {
-        die("Could not query the database: <br />" . $db->error . '<br>Query: ' . $query);
-      }
-
       $i = 1;
       while ($row = $result->fetch_object()) {
         echo '<tr>';
